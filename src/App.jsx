0087be91ed38f8b0e738b9a433eaecda67cf220f
unknown <gabrielswagner7@gmail.com>
2024-07-31 16:34:12 -0500

    score log

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare = {handleSelectSquare} turns={gameTurns}/>
       </div>
-      <Log />
+      <Log turns={gameTurns}/>
     </main>
   )
 }
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,6 +1,7 @@
-function Log() {
+function Log({ turns }) {
     return (
         <ol id="log">
+            {turns.map(turn => <li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row},{turn.square.col}</li>)}
         </ol>
     )
 };
